836fb788450108942c025204ed5f97f0a4e672ca
Alexander Ignatov <alexander.ignatov@narasyst.com>
2023-06-23 07:50:27 +0300

    Using custom arrayList"

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,29 +1,73 @@
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 public class MyArrayList<T> implements Iterable<T> {
-    private List<T> elements;
+    private Node<T> head;
+    private int size;
 
     public MyArrayList() {
-        elements = new ArrayList<>();
+        head = null;
+        size = 0;
     }
 
-    public void add(T element) {
-        elements.add(element);
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newNode);
+        }
+
+        size++;
     }
 
     public T get(int index) {
-        return elements.get(index);
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.getNext();
+        }
+
+        return current.getData();
     }
 
     public void remove(T element) {
-        elements.remove(element);
+        if (head == null) {
+            throw new IllegalArgumentException("Element not found: " + element.toString());
+        }
+
+        if (head.getData().equals(element)) {
+            head = head.getNext();
+            size--;
+            return;
+        }
+
+        Node<T> current = head;
+        Node<T> previous = null;
+        while (current != null) {
+            if (current.getData().equals(element)) {
+                previous.setNext(current.getNext());
+                size--;
+                return;
+            }
+
+            previous = current;
+            current = current.getNext();
+        }
+
+        throw new IllegalArgumentException("Element not found: " + element.toString());
     }
 
     public int size() {
-        return elements.size();
+        return size;
     }
 
     @Override
@@ -32,15 +76,15 @@ public class MyArrayList<T> implements Iterable<T> {
     }
 
     private class MyArrayListIterator implements Iterator<T> {
-        private int currentIndex;
+        private Node<T> current;
 
         public MyArrayListIterator() {
-            currentIndex = 0;
+            current = head;
         }
 
         @Override
         public boolean hasNext() {
-            return currentIndex < elements.size();
+            return current != null;
         }
 
         @Override
@@ -49,9 +93,31 @@ public class MyArrayList<T> implements Iterable<T> {
                 throw new NoSuchElementException();
             }
 
-            T element = elements.get(currentIndex);
-            currentIndex++;
-            return element;
+            T data = current.getData();
+            current = current.getNext();
+            return data;
+        }
+    }
+
+    private static class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            next = null;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
         }
     }
 }
